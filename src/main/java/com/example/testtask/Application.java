517cd610034c516ef28c5a130e66bf459261fd48
egorioo <egorrudenko2002@gmail.com>
2022-06-30 13:08:38 +0300

    test

--- src/main/java/com/example/testtask/Application.java
+++ src/main/java/com/example/testtask/Application.java
@@ -1,5 +1,7 @@
 package com.example.testtask;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,21 +10,13 @@ import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class Application{
-
-//    @Autowired
-//    ApplicationContextProvider applicationContextProvider;
+public class Application implements CommandLineRunner{
 
     public static void main(String[] args) {
        SpringApplication.run(Application.class, args);
     }
 
-//    @Bean
-//    public Test test() {
-//        return new Test();
-//    }
-//
-//    @Override
-//    public void run(String...args) throws Exception {
-//    }
+    @Override
+    public void run(String...args) throws Exception {
+    }
 }
